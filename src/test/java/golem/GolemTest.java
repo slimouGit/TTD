4de1f2c2b1ae733aebf531c52df6b3c214403ec1
Salim Oussayfi <box@slimou.net>
2019-06-08 17:31:32 +0200

    added tests

--- src/test/java/golem/GolemTest.java
+++ src/test/java/golem/GolemTest.java
@@ -17,7 +17,22 @@ public class GolemTest {
     @Test
     public void loesungswortFalsch_tutNichts(){
         Golem golem = new Golem("LOESUNGSWORT");
-        String aktion = golem.bewegeDich("FALSCH", 3, Richtung.NORDEN);
+        String aktion = golem.bewegeDich("FALSCH", 3, Richtung.WESTEN);
         assertEquals("Es passiert Nichts.", aktion);
     }
+
+    @Test
+    public void loesungswortRichtig_bewegtSich1Meter(){
+        Golem golem = new Golem("LOESUNGSWORT");
+        String aktion = golem.bewegeDich("LOESUNGSWORT", 1, Richtung.SUEDEN);
+        assertEquals("Der Golem bewegt sich 1 Schritte nach SUEDEN", aktion);
+    }
+
+    @Test
+    public void loesungswortRichtig_0Meter_tutNichts(){
+        Golem golem = new Golem("LOESUNGSWORT");
+        String aktion = golem.bewegeDich("LOESUNGSWORT", 0, Richtung.SUEDEN);
+        assertEquals("Es passiert Nichts.", aktion);
+    }
+
 }
