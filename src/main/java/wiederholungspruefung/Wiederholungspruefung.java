ba948867cbed153ad15e94cb652ca43457437d92
Salim Oussayfi <box@slimou.net>
2020-02-07 19:35:08 +0100

    Wiederholungspruefung Test

--- src/main/java/wiederholungspruefung/Wiederholungspruefung.java
+++ src/main/java/wiederholungspruefung/Wiederholungspruefung.java
@@ -3,9 +3,43 @@ package wiederholungspruefung;
 import java.time.LocalDate;
 
 public class Wiederholungspruefung {
-    public boolean pruefeWiederholungsPruefungsErfordernis(LocalDate festgestelltAm){
-        System.out.println(festgestelltAm);
-        return true;
 
+    private LocalDate tmpResult;
+
+    /**
+     *
+     * Eine Pruefung ist dann erforderlich, wenn sie folgende Kriterien erfuellt:
+     * 'festgestelltAm' liegt vor dem 01.06.2014 und das berechnete Datum der naechsten Pruefung liegt zwischen
+     * dem 01.06.2019 und dem aktuellen Kalendertag
+     */
+    public boolean pruefeWiederholungsPruefungsErfordernis(LocalDate festgestelltAm) {
+        boolean wiederholungspruefungErforderlich = false;
+        LocalDate untereDatumsGrenze = LocalDate.of(2014, 06, 01);
+        if(festgestelltAm.isBefore(untereDatumsGrenze) && ermittelPruefdatum(festgestelltAm).isBefore(LocalDate.now())){
+                wiederholungspruefungErforderlich = true;
+        }
+        return wiederholungspruefungErforderlich;
+    }
+
+
+    public LocalDate ermittelPruefdatum(LocalDate pruefDatum) {
+        LocalDate ermitteltesDatum = berechnePruefDatum(pruefDatum);
+        return ermitteltesDatum;
+    }
+
+    /**
+     * Die naechste Pruefung muss nach dem 01.06.2019 erfolgen
+     * wenn das pruefDatum vor dem 06.01.2019 liegt, werden solange 5 Jahre addiert
+     * bis diese Bedingung nicht mehr zutrifft
+     */
+    private LocalDate berechnePruefDatum(LocalDate pruefDatum) {
+        LocalDate obereDatumsGrenze = LocalDate.of(2019,06,01);
+        if(pruefDatum.isBefore(obereDatumsGrenze)){
+            this.tmpResult=pruefDatum.plusYears(5);
+            berechnePruefDatum(tmpResult);
+        }else{
+            this.tmpResult=pruefDatum;
+        }
+        return tmpResult;
     }
 }
--- src/test/java/wiederholungspruefung/WiederholungspruefungTest.java
+++ src/test/java/wiederholungspruefung/WiederholungspruefungTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import java.time.LocalDate;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class WiederholungspruefungTest {
 
@@ -15,4 +15,23 @@ public class WiederholungspruefungTest {
         boolean pruefungErforderlich = wiederholungspruefung.pruefeWiederholungsPruefungsErfordernis(pruefDatum);
         assertTrue(pruefungErforderlich);
     }
+
+    @Test
+    public void pruefungNochNichtNotwendig(){
+        Wiederholungspruefung wiederholungspruefung = new Wiederholungspruefung();
+        LocalDate pruefDatum = LocalDate.of(2010,06,01);
+        boolean pruefungErforderlich = wiederholungspruefung.pruefeWiederholungsPruefungsErfordernis(pruefDatum);
+        assertFalse(pruefungErforderlich);
+    }
+
+    @Test
+    public void ermittelPruefdatum(){
+        Wiederholungspruefung wiederholungspruefung = new Wiederholungspruefung();
+        LocalDate erwartetesDatum = LocalDate.of(2023,01,01);
+        LocalDate eingegebenesDatum = LocalDate.of(2008,01,01);
+        LocalDate berechnetesDatum = wiederholungspruefung.ermittelPruefdatum(eingegebenesDatum);
+        assertEquals(erwartetesDatum.toString(), berechnetesDatum.toString());
+    }
+
+
 }
