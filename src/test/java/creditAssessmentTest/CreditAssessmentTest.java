29a3eb5112ec76d8774b9efbf5d38e6954c4521e
Salim Oussayfi <box@slimou.net>
2020-02-23 12:19:13 +0100

    Mockito false

--- src/test/java/creditAssessmentTest/CreditAssessmentTest.java
+++ src/test/java/creditAssessmentTest/CreditAssessmentTest.java
@@ -6,16 +6,25 @@ import creditAssessment.Dept;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class CreditAssessmentTest {
 
+    //https://www.vogella.com/tutorials/Mockito/article.html
     @Test
-    public void checkCreditWorthiness(){
-        assertEquals(new CreditAssessment().creditScreening(new Balance(500),new Dept(200)), CreditAssessment.AssistmentResult.CREDIT_WORTHINESS);
+    public void mockitoTest() {
+        CreditAssessment test = mock(CreditAssessment.class);
+        when(test.creditScreening(new Balance(500), new Dept(200))).thenReturn(CreditAssessment.AssistmentResult.CREDIT_WORTHINESS);
     }
 
     @Test
-    public void checkCreditUnworthiness(){
-        assertEquals(new CreditAssessment().creditScreening(new Balance(200),new Dept(500)), CreditAssessment.AssistmentResult.CREDIT_UNWORTHINESS);
+    public void checkCreditWorthiness() {
+        assertEquals(new CreditAssessment().creditScreening(new Balance(500), new Dept(200)), CreditAssessment.AssistmentResult.CREDIT_WORTHINESS);
+    }
+
+    @Test
+    public void checkCreditUnworthiness() {
+        assertEquals(new CreditAssessment().creditScreening(new Balance(200), new Dept(500)), CreditAssessment.AssistmentResult.CREDIT_UNWORTHINESS);
     }
 }
